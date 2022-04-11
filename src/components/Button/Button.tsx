20079be92c5946d8f0c471ed2c2cb7c8a16baeee
Fedor <fedrdrdr@yandex.ru>
2022-04-11 16:01:45 +0300

    add colors for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,18 +3,25 @@ import classes from "./Button.module.css";
 // import './Button.module.css';
 
 const solidViolet = classes.solidViolet;
-const outlineViolet = classes.outlineViolet;
 const solidYellow = classes.solidYellow;
 const solidRed = classes.solidRed;
+const solidWhite = classes.solidWhite;
+const solidGreen = classes.solidGreen;
+
+const outlineViolet = classes.outlineViolet;
 const outlineRed = classes.outlineRed;
+const outlineBlack = classes.outlineBlack;
+const outlineWhite = classes.outlineWhite;
+const outlineGreen = classes.outlineGreen;
+
 const flatViolet = classes.flatViolet;
 const flatRed = classes.flatRed;
 const flatBlack = classes.flatBlack;
-
+const flatGreen = classes.flatGreen;
 
 export interface ButtonProps {
   variant?: "solid" | "outline" | "flat";
-  color?: "violet" | "red" | "yellow"| "black";
+  color?: "violet" | "red" | "yellow"| "black" | "white" | "green";
   children?: ReactElement|string;
   icon?: string;
   type?: "button" | "submit" | "reset"
@@ -40,12 +47,21 @@ const Button = ({
     // const classButton = classes[className];
     const classButton =
         (variant === "solid" && color === "violet" && solidViolet) ||
-        (variant === "outline" && color === "violet" && outlineViolet) ||
         (variant === "solid" && color === "red" && solidRed) ||
-        (variant === "outline" && color === "red" && outlineRed) ||
         (variant === "solid" && color === "yellow" && solidYellow) ||
+        (variant === "solid" && color === "white" && solidWhite) ||
+        (variant === "solid" && color === "green" && solidGreen) ||
+
+        (variant === "outline" && color === "violet" && outlineViolet) ||
+        (variant === "outline" && color === "red" && outlineRed) ||
+        (variant === "outline" && color === "black" && outlineBlack) ||
+        (variant === "outline" && color === "white" && outlineWhite) ||
+        (variant === "outline" && color === "green" && outlineGreen) ||
+
         (variant === "flat" && color === "violet" && flatViolet) ||
-        (variant === "flat" && color === "red" && flatRed)|| (variant === "flat" && color === "black" && flatBlack);
+        (variant === "flat" && color === "red" && flatRed)|| 
+        (variant === "flat" && color === "green" && flatGreen) ||
+        (variant === "flat" && color === "black" && flatBlack);
 
     const getClassBySize = (size:string|undefined) => {
         switch (size) {
